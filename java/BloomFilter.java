f14b7bb5eb508033180c2399728a2f8edf5a9a7b
Paul B <paul@smart-layers.com>
2022-01-06 15:38:50 +0100

    rm: BloomFilter interface

--- java/BloomFilter.java
+++
@@ -1,9 +0,0 @@
-package com.tsystems.filter;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-public interface BloomFilter {
-    void add(byte[] element) throws NoSuchAlgorithmException, IOException;
-    boolean contains(byte[] element) throws NoSuchAlgorithmException, IOException;
-}
